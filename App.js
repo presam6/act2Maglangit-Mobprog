3c993e250d9341ef32615f8164369f2d21211cca
presam6 <maglangit.mjosh@gmail.com>
2023-12-08 00:11:14 +0800

    from gpt - first

--- App.js
+++ App.js
@@ -1,11 +1,67 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+const CustomButton = ({
+  onPress,
+  title,
+  textColor,
+  width,
+  buttonStyle
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.myButton, {width: width}, buttonStyle]}
+    >
+      <Text style={{ color:textColor }}>{title}</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
+  const [studentInfo, setStudentInfo] = useState({
+    name: '',
+    studentId: '',
+  });
+
+  const showResults = () => {
+    Alert.alert(`Student Name: ${studentInfo.name}\nStudent ID: ${studentInfo.studentId}`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* text */}
+      <View style={styles.textContainer}>
+        <Text style={styles.textTitle}>Act 2 Maglangit</Text>
+        <Text style={styles.subTitle}>Student Information</Text>
+        <StatusBar style="auto" />
+      </View>
+
+      {/* Text inputs */}
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Student Name"
+          onChangeText={(text) => setStudentInfo({ ...studentInfo, name: text })}
+          style={styles.input}
+        />
+
+        <TextInput
+          placeholder="Student ID"
+          onChangeText={(text) => setStudentInfo({ ...studentInfo, studentId: text })}
+          style={styles.input}
+        />
+      </View>
+
+      {/* button */}
+      <View style={styles.arrangeButton}>
+        <CustomButton
+          onPress={showResults}
+          title='Show Results'
+          textColor='black'
+          buttonStyle={{ backgroundColor: 'orange' }}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +69,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  textContainer: {
+    flex: 1,
+    marginTop: 50,
+  },
+  textTitle: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 5,
+  },
+  subTitle: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 5,
+  },
+  inputContainer: {
+    flex: 2,
+    flexDirection: 'column',
+    backgroundColor: 'red',
+    width: '50%',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+  },
+  arrangeButton: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 50,
+  },
+  myButton: {
+    padding: 10,
+    borderRadius: 5,
   },
 });
